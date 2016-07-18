e9cc485be15484d720a4afb9a153ca49f0fbdbea
Lexie <lexie@protonmail.ch>
2016-07-18 00:22:39 -0400

    text pruebaa

--- index.js
+++ index.js
@@ -397,22 +397,3 @@ function managerMessage(recipientId, text) {
     }
     return false;
 };
-
-
-
-
-
-
-
-
-
-function batteryTextMessage(recipientId, text) {
-    text = text || "";
-    text = text.toLowerCase();
-    var values = text.split(' ');
-    if (values[0] === 'battery') {
-            sendMessage(recipientId, {text: "Data.texts().batteryLevel"});
-            return true;
-    }
-    return false;
-};
