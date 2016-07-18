36003483dc0b436a6a82ea2c189800e189274a69
LexieCore <lexie@protonmail.ch>
2016-07-18 04:52:37 -0400

    text pruebaa

--- index.js
+++ index.js
@@ -418,26 +418,42 @@ function batteryImageMessage(recipientId, text) {
 
 
       message = {
-          "attachment": {
-              "type": "template",
-              "payload": {
-                  "template_type": "generic",
-                  "elements": [{
-                      "title": "The Kitten",
-                      "subtitle": "Epic kitten picture",
-                      "image_url": imageUrl ,
-                      "buttons": [{
-                          "type": "web_url",
-                          "url": imageUrl,
-                          "title": "Show kitten"
-                          }, {
-                          "type": "postback",
-                          "title": "I like this " + recipientId,
-                          "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                      }]
-                  }]
-              }
-          }
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "rift",
+            "subtitle": "Next-generation virtual reality",
+            "item_url": "https://www.oculus.com/en-us/rift/",
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.oculus.com/en-us/rift/",
+              "title": "Open Web URL"
+            }, {
+              "type": "postback",
+              "title": "Call Postback",
+              "payload": "Payload for first bubble",
+            }],
+          }, {
+            "title": "touch",
+            "subtitle": "Your Hands, Now in VR",
+            "item_url": "https://www.oculus.com/en-us/touch/",
+            "image_url": "http://messengerdemo.parseapp.com/img/touch.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.oculus.com/en-us/touch/",
+              "title": "Open Web URL"
+            }, {
+              "type": "postback",
+              "title": "Call Postback",
+              "payload": "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
       };
 
             sendMessage(recipientId, message);
